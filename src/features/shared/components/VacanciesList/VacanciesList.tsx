b7a849ae41d09fa14e6f140e1f82f4e4e052b816
Viktar Hameza <viktor.hameza@gmail.com>
2023-05-16 21:42:40 +0300

    feat: add favorites & pagination

--- src/features/shared/components/VacanciesList/VacanciesList.tsx
+++ src/features/shared/components/VacanciesList/VacanciesList.tsx
@@ -1,18 +1,61 @@
+import React from "react";
 import { useSearch } from "./hooks";
 import { VacancyCard } from "./components/VacancyCard";
+import { Pagination, Button } from "@mantine/core";
+import Cookie from "js-cookie";
+import { DEFAULT_SEARCH_PARAMS } from "./hooks";
 
 export const VacanciesList = () => {
-  const { data: { objects: vacancies = [] } = {} } = useSearch();
+  const [page, setPage] = React.useState(DEFAULT_SEARCH_PARAMS.page + 1);
+  const [count] = React.useState(DEFAULT_SEARCH_PARAMS.count);
+  const { data: { objects: vacancies = [], total = 0 } = {} } = useSearch({
+    page: page - 1,
+    count,
+  });
+  const [favorites, setFavorites] = React.useState<Set<number>>(new Set());
+
+  React.useEffect(() => {
+    const favoritesFromCookie = JSON.parse(Cookie.get("favorites") || "[]");
+    setFavorites(new Set(favoritesFromCookie));
+  }, []);
+
+  const pagesTotal = React.useMemo(() => {
+    return Math.ceil(total / count);
+  }, [total, count]);
 
   return (
-    <ul>
-      {vacancies.map((vacancy) => {
-        return (
-          <li key={vacancy.id}>
-            <VacancyCard vacancy={vacancy} />
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <ul>
+        {vacancies.map((vacancy) => {
+          const isFavorite = favorites.has(vacancy.id);
+
+          return (
+            <li key={vacancy.id}>
+              <VacancyCard vacancy={vacancy} />
+              <Button
+                onClick={() => {
+                  isFavorite
+                    ? favorites.delete(vacancy.id)
+                    : favorites.add(vacancy.id);
+                  setFavorites(new Set(favorites));
+                  Cookie.set(
+                    "favorites",
+                    JSON.stringify(Array.from(favorites)),
+                    {
+                      expires: 365,
+                    }
+                  );
+                }}
+              >
+                {isFavorite ? "Remove" : "Add"}
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+      {Boolean(pagesTotal) && (
+        <Pagination value={page} onChange={setPage} total={pagesTotal} />
+      )}
+    </div>
   );
 };
--- src/features/shared/components/VacanciesList/components/VacancyCard.tsx
+++ src/features/shared/components/VacanciesList/components/VacancyCard.tsx
@@ -1,14 +1,17 @@
+import React from "react";
 import { Vacancy } from "../types";
 
 interface VacancyCardProps extends React.HTMLAttributes<HTMLElement> {
   vacancy: Vacancy;
 }
 
-export const VacancyCard = ({ vacancy }: VacancyCardProps) => {
+export const VacancyCard = React.memo(({ vacancy }: VacancyCardProps) => {
   return (
     <div>
       {vacancy.profession} - {vacancy.firm_name} - {vacancy.town.title} -{" "}
       {vacancy.type_of_work.title} - {vacancy.payment_to} - {vacancy.currency}
     </div>
   );
-};
+});
+
+VacancyCard.displayName = "Vacancy Card";
